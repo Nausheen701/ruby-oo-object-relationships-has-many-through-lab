128e72c1697d911e5d2deb2a4e2ddd82b056b360
Nausheen Akhter <nausheen701@gmail.com>
2020-12-06 12:24:38 +0300

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,19 @@
+class Artist
 
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        save
+    end
+
+    def save
+        @@all << self
+      end
+    
+      def self.all
+        @@all
+    
+end
